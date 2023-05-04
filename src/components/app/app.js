b8e98a5717470a2d188d20b8a56bdf2f3e672ec5
Omax <omax1986@gmail.com>
2023-05-04 19:17:29 -0400

    ondelete_employee

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,27 +8,51 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'John C.', salary: 800, increase: false, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+                {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+            ]
+        }
+    }
 
-    const data = [
-        {name: 'John C.', salary: 800, increase: false},
-        {name: 'Alex M.', salary: 3000, increase: true},
-        {name: 'Carl W.', salary: 5000, increase: false},
-    ];
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            // const index = data.findIndex(elem => elem.id === id);
+            
+            // const before = data.slice(0, index);
+            // const after = data.slice(index + 1);
 
-    return (
-        <div className="app">
-            <AppInfo/>
+            // const newArr = [...before, ...after];
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+            return {
+                data: data.filter(item => item.id!== id) 
+            }
+
+        })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppInfo/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+    
+                <EmployersList 
+                    data={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <EmployersAddForm/>
             </div>
-
-            <EmployersList data={data}/>
-            <EmployersAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -24,7 +24,7 @@ class EmployersListItem extends Component {
     }
 
     render() {
-        const {name, salary} = this.props;
+        const {name, salary, onDelete} = this.props;
         const {increase} = this.state;
         const {like} = this.state;
 
@@ -49,7 +49,8 @@ class EmployersListItem extends Component {
                         <i className="fas fa-cookie"></i>
                     </button>
                     <button type="button"
-                        className="btn-trash btn-sm">
+                        className="btn-trash btn-sm"
+                        onClick={onDelete}>
                         <i className="fas fa-trash"></i>
                     </button>
                     <i className="fas fa-star"></i>
