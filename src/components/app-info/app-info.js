a5286d96d97ed0d69451c2a61061e1b5ce2aa2bb
Omax <omax1986@gmail.com>
2023-06-07 19:06:00 -0400

    app_info__optimization

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,12 @@
 import './app-info.css';
 
-const AppInfo = () => {
+const AppInfo = ({employees, increased}) => {
+    
     return (
         <div className="app-info">
             <h1>Accounting of employees in the company N</h1>
-            <h2>Total number of employees:</h2>
-            <h2>Bonuses will have:</h2>
+            <h2>Total number of employees: {employees}</h2>
+            <h2>Bonuses will have: {increased}</h2>
         </div>
     )
 }
